45f83db88d0a31cb72b4fc7085d8636581da5303
Daniel Silver <das2c3@mail.missouri.edu>
2014-12-25 14:47:10 -0600

    Create LinkedListCycles.java

---
+++ LinkedListCycles.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        
+        if (head == null)      return false;
+        if (head.next == null) return false;
+
+        ListNode p1 = head,
+                 p2 = head;
+        
+        do {
+            p1 = p1.next;
+            p2 = p2.next;
+            if (p2 == null)
+                return false;
+
+            p2 = p2.next;
+            if (p1 == null || p2 == null) {
+                return false;
+            }
+        } while (p1 != p2);
+
+        return true;
+    }
+}

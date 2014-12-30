20fe129b299cad3d69ccf7c02f896f92880cbc8d
Daniel Silver <das2c3@mail.missouri.edu>
2014-12-30 11:15:34 -0600

    Create FindSingleNumber.java

---
+++ FindSingleNumber.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * https://oj.leetcode.com/problems/single-number/
+ * 
+ * Given an array of integers, every element appears twice except for one. Find that single one.
+ * 
+ * Solution: iterate through the array using the exclusive xor bit operator.  The second number
+ * will reverse the outcome of the first, so the odd number will be left over.
+ * 
+ **/
+
+public class Solution {
+    public int singleNumber(int[] A) {
+        int num = 0;
+        for (int i = 0; i < A.length; i++)
+            num ^=A[i];
+        return num;
+    }
+}
